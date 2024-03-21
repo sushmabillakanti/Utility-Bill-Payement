74f1b128f49a53731c0e43d54698ec889ae1a785
sushmabillakanti <sushma.billakanti15@gmail.com>
2024-03-21 12:19:39 +0530

    firebase

---
+++ bps-system/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {firestore} from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdcyoPmAg1sJdMueeqE_Sz_MPfNUF46LQ",
+  authDomain: "sample-5aac7.firebaseapp.com",
+  projectId: "sample-5aac7",
+  storageBucket: "sample-5aac7.appspot.com",
+  messagingSenderId: "1050311019563",
+  appId: "1:1050311019563:web:bc0daa2a5a4b97f8fd6e6e",
+  measurementId: "G-Q5ZGM2C5GT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
